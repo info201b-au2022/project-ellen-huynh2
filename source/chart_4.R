fedaab416d29beb04ab7a798817b9ad7eca8f2ed
angmenge <angelamenge@live.com>
2022-11-14 04:03:05 -0800

    chart 4

---
+++ source/chart_4.R
@@ -0,0 +1,24 @@
+#A pie chart displaying the most common occurring categories of crime in Seattle
+#Angela Menge
+
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+install.packages("scales")
+
+source("data_cleaning.R")
+
+#create pie chart
+crime_data %>%
+  count(Offense.Parent.Group) %>%
+  mutate(Offense.Parent.Group = factor(Offense.Parent.Group), percentage = n / sum(nrow(crime_data))) %>%
+  ggplot(aes(x="", y = percentage, fill = Offense.Parent.Group)) +
+  geom_col(color = "black") +
+  coord_polar(theta = "y") +
+  labs(title = "Most Common Categories of Crime in Seattle") +
+  geom_text(aes(label = scales::percent(percentage)),
+            position = position_stack(vjust = 0.5),
+            color = "white") +
+  guides(fill = guide_legend(title = "Offense Categories")) +
+  theme_void()
+
