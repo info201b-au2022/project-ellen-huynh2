bd804fe0b30303160c7617c67e429ca205e9a399
Ellen Huynh <Ellen@Ellens-MacAir17.local>
2022-11-15 10:53:35 -0800

    completed summary table

---
+++ source/table summary info.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(ggplot2)
+
+#source("data_cleaning_small.R")
+source("data_cleaning.R")
+
+#view(crime_data)
+
+summary_table <- crime_data %>%
+  mutate(assign_1 = 1) %>%
+  group_by(year) %>%
+  summarise(
+    number_crimes_that_year = sum(assign_1),
+    most_frequent_crime_that_year = names(which.max(Offense)),
+    most_frequent_neighborhood_that_year = names(which.max(MCPP))
+  )
+    
+  
+  
